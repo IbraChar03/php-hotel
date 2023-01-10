41bb3577983c741d459c4a2fb1df4f43526869ea
Ibrahim <ibrachar68@gmail.com>
2023-01-10 13:50:14 +0100

    creazione tabella

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
     <?php
     $hotels = [
@@ -50,21 +52,37 @@
 </head>
 
 <body>
-    <?php
-    foreach ($hotels as $hotel) {
-        $name = $hotel["name"];
-        $description = $hotel["description"];
-        $parking = $hotel["parking"];
-        $vote = $hotel["vote"];
-        $distance = $hotel["distance_to_center"];
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Distance To Center</th>
 
-        echo "<h1>" . $name . "</h1>";
-        echo "Description : " . $description . "<br>";
-        echo "Rating : " . $vote . "/5" . "<br>";
-        echo "Distance to center : " . $distance . " km" . "<br> <br>";
-    }
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($hotels as $hotel) {
+                $name = $hotel["name"];
+                $description = $hotel["description"];
+                $parking = $hotel["parking"];
+                $vote = $hotel["vote"];
+                $distance = $hotel["distance_to_center"];
+                echo "<tr>";
+                echo "<td>" . $name . "</td>";
+                echo "<td>" . $description . "</td>";
+                echo $parking === false ? "<td>" . "No" . "</td>" : "<td>" . "Yes" . "</td>";
+                echo "<td>" . $vote . "/5" . "</td>";
+                echo "<td>" . $distance . " km" . "</td>";
+                echo "</tr>";
+            }
 
-    ?>
+            ?>
+        </tbody>
+    </table>
 
 </body>
 
